b8d49530da5a5235184d1ed8acacecf3f856440b
KatyaZubareva <abouttkate@mail.ru>
2026-01-11 11:36:43 +0300

    add files from 1 step

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-import './style.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { DateTime } from 'luxon';
+import { DateTime } from "luxon";
+import Modal from "bootstrap/js/dist/modal"; 
 
 const showTimeBtn = document.getElementById('showTimeBtn');
 const modalBody = document.getElementById('modalBody');
-const timeModalEl = document.getElementById('timeModal');
-const timeModal = new bootstrap.Modal(timeModalEl);
+const timeModal = new Modal(document.getElementById('timeModal'));
 
 showTimeBtn.addEventListener('click', () => {
-  const now = DateTime.now().toFormat('dd.MM.yyyy HH:mm:ss');
+  const now = DateTime.now().toFormat("dd.MM.yyyy HH:mm:ss");
   modalBody.innerHTML = `<div class="text-center fs-2">${now}</div>`;
   timeModal.show();
 });
